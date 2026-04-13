21abf3a861e3c74b165e92a16960434cc94ca06d
Dario Salles <dariosalles@gmail.com>
2026-04-12 21:56:57 -0300

    :sparkles: feat: changed base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  //baseURL: 'http://localhost:8080/api'
+  // aws
+  baseURL: 'http://ec2-52-14-78-111.us-east-2.compute.amazonaws.com:8080/api'
 });
 
 // Interceptor para adicionar token de autenticação
